d720abecb3fec08552043f222a41ad2f0ea99b8e
zoeoberon <joan@zoeoberon.com>
2013-08-12 19:28:40 +0000

    add comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,13 @@ module ApplicationHelper
   # --- fetch strings from en.header_text.yml
   
   def get_header_text (params)
+    # options hash:
+    #   page_name: page name prefix to key, e.g. "message.index" for "message.index.h1"
+    #   element: YML key (required)
+    #   add_in: additional text passed through to YML entry
+    #   no_scope: if true, leave :scope option blank on t() call
+    #   no_msg: if true, don't display fix me message if YML entry or default aren't found
+    
     page_name, element, add_in, no_scope, no_msg = params.values_at(:page_name, :element, :add_in, :no_scope, :no_msg)
     return fix_me_statement("element") if element.blank?
    
@@ -16,6 +23,8 @@ module ApplicationHelper
 
    def get_view_text(element)
     return fix_me_statement("element") if element.blank?
+    # assume "lazy" lookup based on calling controller and action
+    #   e.g. ".h1" for view help/index.html.erb rendered by controler/action help/index
     t(".#{element}", default: fix_me_statement(".#{element}"))
   end
 
@@ -37,7 +46,7 @@ module ApplicationHelper
   
   def yield_or_default(section, default="")
     tag_text = content_for(section)
-    # if content_for() not set, check locale file for controller/action default
+    # if content_for() not set, use "lazy" lookup to check locale file for controller/action entry
     if tag_text.blank?
       tag_text = get_header_text(element: section, page_name: "#{controller_name}.#{action_name}", no_scope: true, no_msg: true)
       tag_text = default if tag_text.blank? # use optional default as last resort
