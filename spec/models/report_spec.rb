626cdafa1c51fa4730ba8ccb61267a6479a4ff74
zoeoberon <joan@zoeoberon.com>
2014-12-23 21:52:12 +0000

    update gems, rubocop lint fixes

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Report do
-  it 'has a valid factort with a title, author, published' do
+  it 'has a valid factory with a title, author, published' do
     expect(build(:report)).to be_valid
   end
 end
